b872112943ce8e2fe078859b0d79420fd381d6b4
Un1trix <159701884+Un1trix@users.noreply.github.com>
2024-02-27 11:10:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ moves.forEach((move) => {
         else {
             move.innerText = "X";
             turn0 = true;
-            move.style.color = "#FF2E63";
+            move.style.color = "#white";
         }
         move.disabled = true;
         checkWinner();
